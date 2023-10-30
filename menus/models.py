54425fa8d090271c30b4855cde99c0a96c1523bc
fsouza.andreia <fsouza.andreia@gmail.com>
2023-10-30 12:51:01 +0000

    feat: add MenuItem model; include the app menus; create migration file; create Restaurant and MenuItem tables

--- menus/models.py
+++ menus/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Restaurant(models.Model):
     """ Um objeto classe Restaurant armazena informações
     sobre um restaurante """
@@ -10,3 +11,17 @@ class Restaurant(models.Model):
         """ Retorna uma representação em string do objeto """
 
         return f"[Restaurant] name: {self.name}"
+
+
+class MenuItem(models.Model):
+    """ Um objeto da classe MenuItem armazena informações sobre um item do menu de um restaurante """
+
+    category = models.CharField(max_length=45)
+    description = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """ Retorna uma representação em string do objeto """
+
+        return f"[MenuItem] description: {self.description[:50]}"
